ca41572671f8e0cbad0618bcbfb30bc1087e8bd4
0xAA55 <weixiaobao213@qq.com>
2025-09-03 16:24:10 +0800

    添加初版代码。直接将图片压缩至 320x320，减色，再进行标准化点乘，看点乘值。

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+
+use std::{
+	io::Cursor,
+	path::PathBuf,
+	process::ExitCode,
+};
+use image::{
+	RgbImage,
+	ImageReader,
+	imageops::{
+		resize,
+		FilterType,
+	}
+};
+use turbojpeg::{
+	decompress_image,
+};
+
+const MATCH_SIZE: u32 = 320;
+
+fn load_image(path: &PathBuf) -> RgbImage {
+	let image_data = std::fs::read(path).unwrap();
+	match decompress_image(&image_data) {
+		Ok(img) => img,
+		Err(_) => ImageReader::new(Cursor::new(&image_data)).with_guessed_format().unwrap().decode().unwrap().to_rgb8(),
+	}
+}
+
+fn reduce_color(c: u8) -> u8 {
+	c & 0xF8
+}
+
+fn full_range(half: f32) -> f32 {
+	half * 2.0 - 1.0
+}
+
+fn match_image(img1: &PathBuf, img2: &PathBuf) -> f32 {
+	let img1 = load_image(img1);
+	let img2 = load_image(img2);
+	let img1 = resize(&img1, MATCH_SIZE, MATCH_SIZE, FilterType::Lanczos3);
+	let img2 = resize(&img2, MATCH_SIZE, MATCH_SIZE, FilterType::Lanczos3);
+	let mut sr: f32 = 0.0;
+	let mut sg: f32 = 0.0;
+	let mut sb: f32 = 0.0;
+	for (x, y, p1) in img1.enumerate_pixels() {
+		let p2 = img2.get_pixel(x, y);
+		sr += (full_range(reduce_color(p1.0[0]) as f32 / 255.0) * full_range(reduce_color(p2.0[0]) as f32 / 255.0)).abs() as f32;
+		sg += (full_range(reduce_color(p1.0[1]) as f32 / 255.0) * full_range(reduce_color(p2.0[1]) as f32 / 255.0)).abs() as f32;
+		sb += (full_range(reduce_color(p1.0[2]) as f32 / 255.0) * full_range(reduce_color(p2.0[2]) as f32 / 255.0)).abs() as f32;
+	}
+	(sr.max(sg).max(sb)) / (MATCH_SIZE * MATCH_SIZE) as f32
+}
+
+fn usage() {
+	println!("Usage: imgmatch <image1> <image2>");
+}
+
+fn main() -> ExitCode {
+	let args: Vec<String> = std::env::args().collect();
+	if args.len() < 3 {usage(); return ExitCode::from(1);}
+	
+	let error = match_image(&PathBuf::from(&args[1]), &PathBuf::from(&args[2]));
+	println!("{error}");
+	ExitCode::from(0)
+}
