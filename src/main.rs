7761d00384adf547be380855f2b64bb5220cb416
0xAA55 <weixiaobao213@qq.com>
2025-09-03 18:23:54 +0800

    循环方式改为采取行迭代器进行 `行` 的并行迭代，并添加对 NUMA 架构服务器的优化。

--- src/main.rs
+++ src/main.rs
@@ -36,15 +36,19 @@ fn match_image(img1: &PathBuf, img2: &PathBuf) -> f32 {
 	let mut sr: f32 = 0.0;
 	let mut sg: f32 = 0.0;
 	let mut sb: f32 = 0.0;
-	let rows: Vec<_> = (0..MATCH_SIZE).into_par_iter().map(move |y| {
-		let img1 = img1.clone();
-		let img2 = img2.clone();
+	let rows: Vec<_> = imgs[0].enumerate_rows().zip(imgs[1].enumerate_rows()).map(|((_, row1), (_, row2))| {
+		(row1, row2)
+	}).collect();
+	let rows: Vec<_> = rows.into_par_iter().map(move |(row1, row2)| {
+		#[cfg(feature = "numa")]
+		let (mut row1, mut row2) = (row1.collect::<Vec<_>>().into_iter(), row2.collect::<Vec<_>>().into_iter());
+		#[cfg(not(feature = "numa"))]
+		let (mut row1, mut row2) = (row1, row2);
 		let mut rr: f32 = 0.0;
 		let mut rg: f32 = 0.0;
 		let mut rb: f32 = 0.0;
-		for x in 0..MATCH_SIZE {
-			let p1 = img1.get_pixel(x, y);
-			let p2 = img2.get_pixel(x, y);
+		for _ in 0..MATCH_SIZE {
+			let (p1, p2) = (row1.next().unwrap().2.0, row2.next().unwrap().2.0);
 			rr += ((p1[0] as f32 / 255.0) - (p2[0] as f32 / 255.0)).abs() as f32;
 			rg += ((p1[1] as f32 / 255.0) - (p2[1] as f32 / 255.0)).abs() as f32;
 			rb += ((p1[2] as f32 / 255.0) - (p2[2] as f32 / 255.0)).abs() as f32;
