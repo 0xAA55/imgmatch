cf0235172d4bbfbb0064c77e5be93cd144ab541f
0xAA55 <weixiaobao213@qq.com>
2025-09-03 16:57:32 +0800

    改为按行并行处理。

--- src/main.rs
+++ src/main.rs
@@ -52,11 +52,32 @@ fn match_image(img1: &PathBuf, img2: &PathBuf) -> f32 {
 	let mut sr: f32 = 0.0;
 	let mut sg: f32 = 0.0;
 	let mut sb: f32 = 0.0;
-	for (x, y, p1) in img1.enumerate_pixels() {
-		let p2 = img2.get_pixel(x, y);
-		sr += (full_range(reduce_color(p1.0[0]) as f32 / 255.0) * full_range(reduce_color(p2.0[0]) as f32 / 255.0)).abs() as f32;
-		sg += (full_range(reduce_color(p1.0[1]) as f32 / 255.0) * full_range(reduce_color(p2.0[1]) as f32 / 255.0)).abs() as f32;
-		sb += (full_range(reduce_color(p1.0[2]) as f32 / 255.0) * full_range(reduce_color(p2.0[2]) as f32 / 255.0)).abs() as f32;
+	let mut cols: Vec<_> = (0..MATCH_SIZE).map(|y| {
+		let img1 = img1.clone();
+		let img2 = img2.clone();
+		spawn(move ||{
+			let mut cr: f32 = 0.0;
+			let mut cg: f32 = 0.0;
+			let mut cb: f32 = 0.0;
+			for x in 0..MATCH_SIZE {
+				let p1 = img1.get_pixel(x, y);
+				let p2 = img2.get_pixel(x, y);
+				cr += (full_range(reduce(p1[0]) as f32 / 255.0) * full_range(reduce(p2[0]) as f32 / 255.0)).abs() as f32;
+				cg += (full_range(reduce(p1[1]) as f32 / 255.0) * full_range(reduce(p2[1]) as f32 / 255.0)).abs() as f32;
+				cb += (full_range(reduce(p1[2]) as f32 / 255.0) * full_range(reduce(p2[2]) as f32 / 255.0)).abs() as f32;
+			}
+			(cr, cg, cb)
+		})
+	}).collect();
+	loop {
+		if let Some(job) = cols.pop() {
+			let (cr, cg, cb) = job.join().unwrap();
+			sr += cr;
+			sg += cg;
+			sb += cb;
+		} else {
+			break;
+		}
 	}
 	((sr + sg + sb) / (MATCH_SIZE * MATCH_SIZE * 3) as f32).max(0.0)
 }
